94f11f7a7cc4451830ebf561af169076ba27f92f
Jorge O. Castro <jorge.castro@gmail.com>
2025-04-20 09:42:13 -0400

    fix(docker): load iptable_nat
    So that docker-in-docker works

--- build_files/20-install-apps.sh
+++ build_files/20-install-apps.sh
@@ -57,3 +57,11 @@ dnf5 install -y --enable-repo="docker-ce-stable" "${docker_pkgs[@]}" || {
         dnf5 install -y --enablerepo="docker-ce-test" "${docker_pkgs[@]}"
     fi
 }
+
+# Load iptable_nat module for docker-in-docker.
+# See:
+#   - https://github.com/ublue-os/bluefin/issues/2365
+#   - https://github.com/devcontainers/features/issues/1235
+mkdir -p /etc/modules-load.d && cat >>/etc/modules-load.d/ip_tables.conf <<EOF
+iptable_nat
+EOF
