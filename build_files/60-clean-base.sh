a45726e3ffbf00381d7f66b89ea95a6b9207434a
Jorge O. Castro <jorge.castro@gmail.com>
2025-05-01 21:17:57 -0400

    fix(just): remove outdated recipe

--- build_files/60-clean-base.sh
+++ build_files/60-clean-base.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
 set -xeuo pipefail
-
-for recipe in "install-docker"; do
-  if ! grep -l "^$recipe:" /usr/share/ublue-os/just/*.just | grep -q .; then
-    echo "Error: Recipe $recipe not found in any just file"
-    exit 1
-  fi
-  sed -i "s/^$recipe:/_$recipe:/" /usr/share/ublue-os/just/*.just
-done
